97fa752a91fc568927a0489dc943ee5fe6bd94d8
stanleyshisoka <stanleyshisokanaira@gmail.com>
2025-10-01 23:10:06 +0300

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,8 +3,4 @@ import os
 bind = "0.0.0.0:" + os.environ.get("PORT", "10000")
 workers = 2
 threads = 2
-worker_class = "sync"
 timeout = 120
-max_requests = 1000
-max_requests_jitter = 100
-preload_app = True
